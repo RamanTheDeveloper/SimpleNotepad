342a64357679692b4c4294914a5d27f82bdbd5f0
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2020-12-15 19:31:55 +0100

    Created attributes in DocumentModel and FormatModel

--- SimpleNotepad/Models/FormatModel.cs
+++ SimpleNotepad/Models/FormatModel.cs
@@ -1,10 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
+using System.Windows.Media;
 
 namespace SimpleNotepad.Models
 {
-    public class FormatModel
+    public class FormatModel : ObservableObject
     {
+        private FontStyle _style;
+        public FontStyle Style
+        {
+            get { return _style; }
+            set { OnPropertyChanged(ref _style, value); }
+        }
+
+        private FontWeight _weight;
+        public FontWeight Weight
+        {
+            get { return _weight; }
+            set { OnPropertyChanged(ref _weight, value); }
+        }
+
+        private FontFamily _family;
+        public FontFamily Family
+        {
+            get { return _family; }
+            set { OnPropertyChanged(ref _family, value); }
+        }
+
+        private TextWrapping _wrap;
+        public TextWrapping Wrap
+        {
+            get { return _wrap; }
+            set
+            {
+                OnPropertyChanged(ref _wrap, value);
+                isWrapped = value == TextWrapping.Wrap ? true : false;
+            }
+        }
+
+        private bool _isWrapped;
+        public bool isWrapped
+        {
+            get { return _isWrapped; }
+            set { OnPropertyChanged(ref _isWrapped, value); }
+        }
+
+        private double _size;
+        public double size
+        {
+            get { return _size; }
+            set { OnPropertyChanged(ref _size, value); }
+        }
     }
 }
