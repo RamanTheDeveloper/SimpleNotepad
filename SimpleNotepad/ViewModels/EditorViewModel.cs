987999cbe255242bf4d7e293e461ff26cf3bbe08
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2020-12-15 19:43:56 +0100

    RelayCommand configured and as well EditorViewModel setup

---
+++ SimpleNotepad/ViewModels/EditorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SimpleNotepad.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace SimpleNotepad.ViewModels
+{
+    public class EditorViewModel
+    {
+        public ICommand FormatCommand { get; }
+        public ICommand WrapCommand { get; }
+        public FormatModel Format { get; set; }
+        public DocumentModel Document { get; set; }
+    }
+}
